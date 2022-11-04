9c344433aa62a1e80b2ead324be10130cf38767c
JJ1104 <jithinjose1104@gmail.com>
2022-11-04 16:47:07 +0530

    W8L8

--- Lab_7/Q2.c
+++ Lab_7/Q2.c
@@ -7,62 +7,59 @@ typedef struct Queue{
 }Queue;
 
 void insertcq(Queue* q,int ele,int i){
-    if((q->rear[i-1]+1) == q->capacity/2){
-        printf("Queue is Full!\n");
-        return;
+    if(q->front[i-1] == -1){
+        (q->front[i-1])++;
     }
-    if(q->front[0] == -1){
-        q->front[0] = 0;
+    if(q->front[i-1] == (q->capacity/2)-1){
+        (q->front[i-1])++;
     }
-    if(q->front[1] == q->capacity/2){
-        q->front[1] = q->capacity+1;
-    }
-    q->rear[i-1] = (q->rear[i-1]+1)%q->capacity/2;
-    q->queue[q->rear] = ele;
+    q->rear[i-1] = (q->rear[i-1]+1)%(i*q->capacity/2);
+    q->queue[q->rear[i-1]] = ele;
 }
 
-int deletecq(Queue* q){
-    if(q->front == -1){
+int deletecq(Queue* q,int i){
+    if(q->front[i-1] == q->rear[i-1]){
         return -1;
     }
     
-    int ele = q->queue[q->front];
-    q->front = (q->front+1)%q->capacity;
+    int ele = q->queue[q->front[i-1]];
+    q->front[i-1] = (q->front[i-1]+1)%q->capacity;
     return ele;
 }
 
-void displaycq(Queue* q){
-    for(int i=q->front;i<q->rear+1;i++){
-        printf("%d ",q->queue[i]);
+void displaycq(Queue* q,int i){
+    printf("Displaying Queue %d: ",i);
+    for(int j=q->front[i-1];j<q->rear[i-1]+1;j++){
+        printf("%d ",q->queue[j]);
     }
+    printf("\n");
 }
 
-int find_ele(Queue* q,int ele){
-    for(int i =q->front;i<q->rear+1;i++){
-        if(q->queue[i] == ele){
-            return 1;
-        }
-    }
-    return 0;
-}
 
 int main(){
     Queue *q,que;
     q = &que; 
-    printf("Enter the capacity of queue: ");
-    scanf("%d",&q->capacity);   
-    q->queue = (int*)malloc(q->capacity*sizeof(int));
-    q->front=q->rear=-1;
-    insertcq(q,1);
-    insertcq(q,2);
-    insertcq(q,3);
-    insertcq(q,4);
-    if(find_ele(q,3)){
-        printf("Element Found\n");
+    int num;
+    int size;
+    printf("Enter the number of queues: ");
+    scanf("%d",&num);
+    printf("Enter the capacity of each queue: ");
+    scanf("%d",&size);
+    q->capacity = num*size;   
+    q->queue = (int*)malloc((num*q->capacity)*sizeof(int));
+    q->front = (int*)malloc(num*sizeof(int));
+    q->rear = (int*)malloc(num*sizeof(int));
+
+    for(int i=0;i<num;i++){
+        q->front[i] = q->rear[i] = (i*size)-1;
     }
-    else{
-        printf("Element not found\n");
-    }
-    displaycq(q);
-    printf("\nDeleted element: %d",deletecq(q));
+    insertcq(q,1,1);
+    insertcq(q,2,1);
+    displaycq(q,1);
+    deletecq(q,1);
+    displaycq(q,1);
+
+    insertcq(q,3,2);
+    insertcq(q,4,2);
+    displaycq(q,2);
 }
